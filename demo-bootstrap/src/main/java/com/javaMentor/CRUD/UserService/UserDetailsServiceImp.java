071be2cd0835f1d434c898322f442d7ee91595eb
vstrkv <37896140+vstrkv@users.noreply.github.com>
2021-05-17 02:22:56 +0700

    Add files via upload

---
+++ demo-bootstrap/src/main/java/com/javaMentor/CRUD/UserService/UserDetailsServiceImp.java
@@ -0,0 +1,31 @@
+package com.javaMentor.CRUD.UserService;
+
+import com.javaMentor.CRUD.model.Role;
+import com.javaMentor.CRUD.model.User;
+import com.javaMentor.CRUD.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        if (login.equals("ADMIN")) {
+            User user = new User("ADMIN", "ADMIN", "adm", "adm");
+            user.addRole(new Role(2, "ADMIN"));
+            if (userRepository.findByLogin("ADMIN") == null) {
+                userRepository.save(user);
+            }
+            return user;
+        }
+        return userRepository.findByLogin(login);
+    }
+}
---
+++ demo-bootstrap/src/main/java/com/javaMentor/CRUD/UserService/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.javaMentor.CRUD.UserService;
+
+
+import com.javaMentor.CRUD.model.User;
+import com.javaMentor.CRUD.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service("userService")
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final UserConverter userConverter;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(int id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public User findByLogin(String login) {
+        return userRepository.findByLogin(login);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUser(int id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userRepository.save(user);
+    }
+
+
+}
