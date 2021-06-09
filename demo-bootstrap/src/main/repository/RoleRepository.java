201c3e92ab6b50e85461b573bff7b9a0f2da602d
vstrkv <37896140+vstrkv@users.noreply.github.com>
2021-06-09 16:06:02 +0700

    Delete demo-bootstrap/src/main directory

--- demo-bootstrap/src/main/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javaMentor.CRUD.repository;
-
-import com.javaMentor.CRUD.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
